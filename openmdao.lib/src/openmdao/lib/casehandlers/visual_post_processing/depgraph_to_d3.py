46040a543fed798cf01228c73445d2c6e211933a
Herb Schilling <hschilling@nasa.gov>
2014-08-28 16:33:41 -0400

    works but no arrowheads

--- openmdao.lib/src/openmdao/lib/casehandlers/visual_post_processing/depgraph_to_d3.py
+++ openmdao.lib/src/openmdao/lib/casehandlers/visual_post_processing/depgraph_to_d3.py
@@ -338,34 +338,8 @@ for control_points in spline_data:
                 .style("stroke", "black")
                 .style("fill", "none");'''
 
-
-
-
-
-
-
 contents += '''
 
-
-
-
-
-//The data for our line
- //var lineData = [ { "x": 1,   "y": 5},  { "x": 20,  "y": 20},
-  //                { "x": 40,  "y": 10}, { "x": 60,  "y": 40},
-   //               { "x": 80,  "y": 5},  { "x": 100, "y": 60}];
- 
-
-
-       //force.on("tick", tick);
-
-  //      function tick() {
-   //       link.attr("x1", function(d) { return xscale(2); })
-  //        .attr("y1", function(d) { return yscale(2); })
-  //        .attr("x2", function(d) { return xscale(4); })
-   //       .attr("y2", function(d) { return yscale(4); });
-
-     //   }
 
 };
 
