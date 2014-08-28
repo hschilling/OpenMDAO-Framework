4bfdf522df26153cc8a894a2cb09842f1d3e4865
Herb Schilling <hschilling@nasa.gov>
2014-08-28 16:32:53 -0400

    works but no arrowheads

--- openmdao.lib/src/openmdao/lib/casehandlers/visual_post_processing/depgraph_to_d3.py
+++ openmdao.lib/src/openmdao/lib/casehandlers/visual_post_processing/depgraph_to_d3.py
@@ -201,6 +201,7 @@ with open("graph.plain", "r") as f:
   edge_content = ''
   node_locations = {}
   edge_datas = []
+  spline_data = []
   for i, line in enumerate(f):
     if line.startswith( "node"):
         # node name x y width height label style shape color fillcolor
@@ -218,17 +219,17 @@ with open("graph.plain", "r") as f:
             tail = tail[1:-1]
         if head.startswith('"'):
             head = head[1:-1]
-        if tail == '_pseudo_1.in2' and head == '_pseudo_1':
-            edge_data = 'var lineData = ['
-            control_points = []
-            for i in range(int(n)):
-                #edge_data += '{ "x": xscale(%s),   "y": yscale(%s)},\n' % ( line.split()[4+i], line.split()[5+i] )
-                edge_data += '{ "x": %s,   "y": %s},\n' % ( line.split()[4+i], line.split()[5+i] )
-                control_points.append( (line.split()[4+2*i], line.split()[5+2*i]) )
-            edge_data += ']; // ' + tail + ' ' + head 
-            edge_datas.append( edge_data )
-            #edge_content += '{sourcex: %s ,sourcey: %s, targetx: %s, targety: %s},\n' % ( node_locations[tail][0], node_locations[tail][1], node_locations[head][0], node_locations[head][1] )
-            edge_content += '{source: "%s" , target: "%s"},\n' % ( tail, head )
+        edge_data = 'var lineData = ['
+        control_points = []
+        for i in range(int(n)):
+            #edge_data += '{ "x": xscale(%s),   "y": yscale(%s)},\n' % ( line.split()[4+i], line.split()[5+i] )
+            edge_data += '{ "x": %s,   "y": %s},\n' % ( line.split()[4+i], line.split()[5+i] )
+            control_points.append( (line.split()[4+2*i], line.split()[5+2*i]) )
+        spline_data.append( control_points )
+        edge_data += ']; // ' + tail + ' ' + head 
+        edge_datas.append( edge_data )
+        #edge_content += '{sourcex: %s ,sourcey: %s, targetx: %s, targety: %s},\n' % ( node_locations[tail][0], node_locations[tail][1], node_locations[head][0], node_locations[head][1] )
+        edge_content += '{source: "%s" , target: "%s"},\n' % ( tail, head )
 
 contents += node_content
 
@@ -309,34 +310,33 @@ contents += '''
         '''
 
 #### edges ####
-for edge_data in edge_datas:
-    contents += edge_data
-    contents += '''
-        var lineGraph = svg.append("path")
-                            .attr("d", lineFunction(lineData))
-                            .attr("stroke", "blue")
-                            .attr("stroke-width", 2)
-                            .attr("fill", "none");
-    '''
+# for edge_data in edge_datas:
+#     contents += edge_data
+#     contents += '''
+#         var lineGraph = svg.append("path")
+#                             .attr("d", lineFunction(lineData))
+#                             .attr("stroke", "blue")
+#                             .attr("stroke-width", 2)
+#                             .attr("fill", "none");
+#     '''
 
 
 
+for control_points in spline_data:
+    contents += '''svg.append("svg:path")
+                .attr("d","'''
 
-contents += '''svg.append("svg:path")
-            .attr("d","'''
-
-contents += 'M " + xscale(%s) + " " + yscale(%s) + " "' % control_points[0]
-num_bezier = ( len(control_points) - 1 ) / 3
-import pdb; pdb.set_trace()
-for ib in range(num_bezier):
-    if ib == 0 :
-        contents += ' + "C " + xscale(%s) + " " + yscale(%s) + " " + xscale(%s) + " " + yscale(%s) + " " + xscale(%s) + " " + yscale(%s) ' % ( control_points[ 1 + 3 * ib ] + control_points[ 2 + 3 * ib ] + control_points[ 3 + 3 * ib ] )
-    else:
-        contents += ' + " " + xscale(%s) + " " + yscale(%s) + " " + xscale(%s) + " " + yscale(%s) + " " + xscale(%s) + " " + yscale(%s) ' % ( control_points[ 1 + 3 * ib ] + control_points[ 2 + 3 * ib ] + control_points[ 3 + 3 * ib ] )
-contents += ''')
-            .style("stroke-width", 2)
-            .style("stroke", "orange")
-            .style("fill", "none");'''
+    contents += 'M " + xscale(%s) + " " + yscale(%s) + " "' % control_points[0]
+    num_bezier = ( len(control_points) - 1 ) / 3
+    for ib in range(num_bezier):
+        if ib == 0 :
+            contents += ' + "C " + xscale(%s) + " " + yscale(%s) + " " + xscale(%s) + " " + yscale(%s) + " " + xscale(%s) + " " + yscale(%s) ' % ( control_points[ 1 + 3 * ib ] + control_points[ 2 + 3 * ib ] + control_points[ 3 + 3 * ib ] )
+        else:
+            contents += ' + " " + xscale(%s) + " " + yscale(%s) + " " + xscale(%s) + " " + yscale(%s) + " " + xscale(%s) + " " + yscale(%s) ' % ( control_points[ 1 + 3 * ib ] + control_points[ 2 + 3 * ib ] + control_points[ 3 + 3 * ib ] )
+    contents += ''')
+                .style("stroke-width", 2)
+                .style("stroke", "black")
+                .style("fill", "none");'''
 
 
 
@@ -345,15 +345,6 @@ contents += ''')
 
 
 contents += '''
-
-
-        svg.append("svg:path")
-            .attr("d"," M 0 60 L 50 110 L 90 70 L 140 100")
-            .style("stroke-width", 2)
-            .style("stroke", "orange")
-            .style("fill", "none");
-
-
 
 
 
